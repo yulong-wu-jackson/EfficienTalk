af309a659d37bdf8f9eb4869dbbba208e8f108aa
TonyYu123456 <shijun.yu@mail.utoronto.ca>
2023-10-23 14:26:38 -0400

    Message Committed first time

--- src/entity/Message.java
+++ src/entity/Message.java
@@ -2,4 +2,14 @@ package entity;
 
 public interface Message {
 
+    String getContent();
+
+    String getTimestamp();
+
+    User getSender();
+
+    User getReceiver();
+
+    Boolean getIs_read();
+
 }
