bddbf14881bf38611e56097826c48e65f74d32d7
Yulong <jackson.yl.wu@outlook.com>
2023-11-08 16:35:54 -0500

    working on multithreading of server and client...

--- src/app/Client.java
+++ src/app/Client.java
@@ -15,34 +15,74 @@ public class Client {
     }
 
     public void start(){
-        try {
-            // send message to server
+        // create a thread to handle the data from server
+        ServerHandler serverHandler = new ServerHandler(socket);
+        Thread thread = new Thread(serverHandler);
+        thread.start();
+
+
+        // send message to server
+        try{
             OutputStream outputStream =  socket.getOutputStream();
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "utf-8");
             BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
             // 自动行刷新
             PrintWriter printWriter = new PrintWriter(bufferedWriter, true);
 
-            // receive message from server
-            InputStream inputStream = socket.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
             Scanner scanner = new Scanner(System.in);
             while(true){
                 String message = scanner.nextLine();
-                // 输出客户端的内容
+                // client send its message to server
                 printWriter.println(message);
-
-                // receive message from server
-                String line = bufferedReader.readLine();
-                System.out.println("Server said: " + line);
             }
-
-        } catch (IOException e) {
+        }catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
+
+    class ServerHandler implements Runnable{
+        Socket socket;
+        String host;
+        public ServerHandler(Socket socket){
+            this.socket = socket;
+            this.host = socket.getInetAddress().getHostAddress();
+        }
+        @Override
+        public void run(){
+            try {
+                // send message to server
+                OutputStream outputStream =  socket.getOutputStream();
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "utf-8");
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+                // 自动行刷新
+                PrintWriter printWriter = new PrintWriter(bufferedWriter, true);
+
+
+                Scanner scanner = new Scanner(System.in);
+                // receive message from server
+                InputStream inputStream = socket.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                while(true){
+                    String message = scanner.nextLine();
+                    // client send its message to server
+                    printWriter.println(message);
+
+                    // receive message from server
+                    String line = bufferedReader.readLine();
+                    System.out.println("Server said: " + line);
+                }
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
 
 
     public static void main(String[] args){
--- src/app/Server.java
+++ src/app/Server.java
@@ -17,38 +17,60 @@ public class Server {
     }
 
     public void start(){
-        System.out.println("Waiting Client to connect...");
-
-        // Listen whether a client want to connect to the port, waiting until Client connect
+        // Listen whether a client want to connect to the port, one Client connect, create one Socket.
         try {
-            Socket socket = serverSocket.accept();
-            System.out.println("One Client connected!");
-
-            // get data (stream of bytes) from client
-            InputStream inputStream = socket.getInputStream();
-
-            // convert bytes to string (utf-8)
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-            OutputStream outputStream = socket.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "utf-8");
-            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-            PrintWriter printWriter = new PrintWriter(bufferedWriter, true);
-            Scanner scanner = new Scanner(System.in);
-
             while(true){
-                // receive message from client
-                String line = bufferedReader.readLine();
-                System.out.println("Client said: " + line);
+                System.out.println("Waiting Client to connect...");
+                Socket socket = serverSocket.accept();
+                System.out.println("One Client connected!");
 
-                // send message to client
-                String message = scanner.nextLine();
-                printWriter.println(message);
+                // give the Socket to a thread
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    class ClientHandler implements Runnable{
+        Socket socket;
+        String host;
+        public ClientHandler(Socket socket){
+            this.socket = socket;
+            this.host = socket.getInetAddress().getHostAddress();
+        }
+        @Override
+        public void run(){
+            try {
+                // get data (stream of bytes) from client
+                InputStream inputStream = socket.getInputStream();
+
+                // convert bytes to string (utf-8)
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                OutputStream outputStream = socket.getOutputStream();
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "utf-8");
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+                PrintWriter printWriter = new PrintWriter(bufferedWriter, true);
+                Scanner scanner = new Scanner(System.in);
+
+                while(true){
+                    // receive message from client
+                    String line = bufferedReader.readLine();
+                    System.out.println("Client " + host + " said: " + line);
+
+                    // send message to client
+                    String message = scanner.nextLine();
+                    printWriter.println(message);
+                }
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
         }
     }
 
