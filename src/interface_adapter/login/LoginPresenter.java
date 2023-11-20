885d9e2c703f9575de630fbdab48726c3350f28b
Yulong <jackson.yl.wu@outlook.com>
2023-11-19 20:53:04 -0500

    handled failed to login case, with a pop up window

--- src/interface_adapter/login/LoginPresenter.java
+++ src/interface_adapter/login/LoginPresenter.java
@@ -66,6 +66,8 @@ public class LoginPresenter implements LoginOutputBoundary {
     public void prepareFailView(String error) {
         LoginState loginState = loginViewModel.getState();
         loginState.setUsernameError(error);
+        loginViewModel.setState(loginState);
         loginViewModel.firePropertyChanged();
+        JOptionPane.showMessageDialog(null, "Login failed.");
     }
 }
