c62e2299fb5842c4b6c93d0a03ce82f3f35ddc29
TianleiP <pangtl@126.com>
2023-11-24 22:44:57 -0500

    Added failed sending email Pop-up window

--- src/use_case/notify/NotifyUserDataAccessInterface.java
+++ src/use_case/notify/NotifyUserDataAccessInterface.java
@@ -1,10 +1,13 @@
 package use_case.notify;
 import java.util.ArrayList;
+import java.util.Map;
 
 
 public interface NotifyUserDataAccessInterface {
 
     ArrayList<String> getUserEmails();
     ArrayList<String> getUsernames();
+
+    Map<String,String> getUsersAndEmails();
 
 }
