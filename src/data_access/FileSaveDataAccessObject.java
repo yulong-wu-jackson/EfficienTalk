61eb36fc6e715d9065d9256de0099f4f15884a6c
Qixuan Chu <1933503039@qq.com>
2023-12-02 15:41:43 -0500

    modify the two save methods separetly in save use case and summary use case, now they can be saved lines by lines, instead of just in one line, and the file name will be related to the real-time time, which help for human viewing.

--- src/data_access/FileSaveDataAccessObject.java
+++ src/data_access/FileSaveDataAccessObject.java
@@ -5,9 +5,12 @@ import use_case.save.SaveUserDataAccessInterface;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class FileSaveDataAccessObject implements SaveUserDataAccessInterface {
     public FileSaveDataAccessObject(){};
+
     public static void saveStringToFile(String content, String filePath) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             writer.write(content);
@@ -16,16 +19,36 @@ public class FileSaveDataAccessObject implements SaveUserDataAccessInterface {
             e.printStackTrace();
         }
     }
+
     @Override
     public void saveMessage(String savedMessage) {
-        String filePath = "savedMessage.txt";
+        String formattedMessage = formatMessageWithLineBreaks(savedMessage, 110);
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmm"));
+        String filePath = "savedMessage_" + timestamp + ".txt";
 
         try {
-            saveStringToFile(savedMessage, filePath);
+            saveStringToFile(formattedMessage, filePath);
             System.out.println("Content saved to file: " + filePath);
         } catch (IOException e) {
             System.err.println("An error occurred while saving the file.");
             e.printStackTrace();
         }
+    }
+
+    private String formatMessageWithLineBreaks(String message, int lineLength) {
+        StringBuilder formattedMessage = new StringBuilder();
+        int counter = 0;
+
+        for (char ch : message.toCharArray()) {
+            if (counter >= lineLength && ch == ' ') {
+                formattedMessage.append('\n');
+                counter = 0;
+            } else {
+                formattedMessage.append(ch);
+                counter++;
+            }
+        }
+
+        return formattedMessage.toString();
     }
 }
--- src/data_access/FileSummayDataAccessObject.java
+++ src/data_access/FileSummayDataAccessObject.java
@@ -3,9 +3,10 @@ package data_access;
 import use_case.summary.SummaryUserDataAccessInterface;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class FileSummayDataAccessObject implements SummaryUserDataAccessInterface {
     public FileSummayDataAccessObject(){};
@@ -20,15 +21,33 @@ public class FileSummayDataAccessObject implements SummaryUserDataAccessInterfac
 
     @Override
     public void saveSummary(String summary) {
-        String filePath = "summary.txt";
+        String formattedMessage = formatMessageWithLineBreaks(summary, 110);
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmm"));
+        String filePath = "summary_" + timestamp + ".txt";
 
         try {
-            saveStringToFile(summary, filePath);
+            saveStringToFile(formattedMessage, filePath);
             System.out.println("Content saved to file: " + filePath);
         } catch (IOException e) {
             System.err.println("An error occurred while saving the file.");
             e.printStackTrace();
         }
+    }
 
+    private String formatMessageWithLineBreaks(String message, int lineLength) {
+        StringBuilder formattedMessage = new StringBuilder();
+        int counter = 0;
+
+        for (char ch : message.toCharArray()) {
+            if ((counter >= lineLength && ch == ' ') || counter >= lineLength + 10) {
+                formattedMessage.append('\n');
+                counter = 0;
+            } else {
+                formattedMessage.append(ch);
+                counter++;
+            }
+        }
+
+        return formattedMessage.toString();
     }
 }
